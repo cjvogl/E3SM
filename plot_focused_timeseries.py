bc2cadfbe0f57920ed31384288eb84ddb7a2c9c2
Chris Vogl <vogl2@llnl.gov>
2019-03-11 20:18:52 -0700

    updated plot focused timeseries script

--- plot_focused_timeseries.py
+++ plot_focused_timeseries.py
@@ -7,19 +7,22 @@ import os.path
 
 #dtList = [4,8]
 dtList = [4]
-config = 'cnvg_condensation/run/RKZ_SGR_qv1_ql1_Al1_lmt4_adjIC_ne30_ne30_FC5AQUAP_intel_quartz'
+config = 'cnvg_condensation/run/RKZ_SGR_extrapf_qv1_ql1_Al1_lmt4_adjIC_ne30_ne30_FC5AQUAP_intel_quartz'
 timeList = [0, 1800, 3600, 5400, 7200]
 variableList = ['T','Q','CLDLIQ','RKZ_RH',\
                 'RKZ_AT','RKZ_Al','RKZ_Av','RKZ_fac', \
                 'RKZ_dqsatdT', 'RKZ_zqme', 'RKZ_qme',\
                 'RKZ_qlneg_lm4', 'RKZ_qvneg_lm4']
-variableList = ['CLDLIQ','RKZ_fac','RKZ_qme','RKZ_Al','Q', 'T', 'RKZ_Av','RKZ_RH', 'RKZ_qlneg_lm4', 'RKZ_qvneg_lm4']
-variableList = ['CLDLIQ','RKZ_fac','RKZ_qme']
+variableList = ['CLDLIQ','RKZ_fac','RKZ_qme','RKZ_Al','RKZ_term_A','RKZ_term_B','RKZ_term_C','nonphysical','RKZ_qlneg_lm4', 'RKZ_qvneg_lm4']
 plotList = variableList
 
-ind = 962360
+#ind = 962360
 #ind = 963667
 #ind = 746876
+#ind = 1125452
+#ind = 1069850
+#ind = 855579
+ind = 746876
 
 solutionDict = {}
 for var in variableList:
@@ -46,11 +49,17 @@ for var in variableList:
         numSlices = len(t_current)
         q_current = np.empty(numSlices)
         print ' Reading in ' + var + '...',
-        tmp = data[var][:]
-        print ' done'
-        for i in range(numSlices):
-          flat = tmp[i,:,:].flatten()
-          q_current[i] = flat[ind]
+        if (var is 'nonphysical'):
+          ql = data['CLDLIQ'][:]
+          f = data['RKZ_fac'][:]
+          for i in range(numSlices):
+            q_current[i] = np.count_nonzero(np.where(f[i,:,:] < 1.0e-12, ql[i,:,:], 0))
+        else:
+          tmp = data[var][:]
+          print ' done'
+          for i in range(numSlices):
+            flat = tmp[i,:,:].flatten()
+            q_current[i] = flat[ind]
         t = np.hstack((t, t_current))
         q = np.hstack((q, q_current))
       tmp = np.vstack((t, q))
@@ -61,37 +70,43 @@ for var in variableList:
   if (var in plotList):
     ax.set_title(var + '(%d)' % ind)
     ax.legend()
-
-#Av = solutionDict['RKZ_Av']
-#dqsatdT = solutionDict['RKZ_dqsatdT']
-#AT = solutionDict['RKZ_AT']
-#gam = solutionDict['gam']
-#gamma = 1.0 + gam
-#Al = solutionDict['RKZ_Al']
-#RH = solutionDict['RKZ_RH']
-#dfdRH = solutionDict['RKZ_dfdRH']
-#qv = solutionDict['Q']
-#ql = solutionDict['CLDLIQ']
-#qsat = qv/RH
-#zforcing = (Av - RH*dqsatdT*AT)/qsat
-#zc3 = (1.0 + gam*RH*dqsatdT)/qsat
-#D = Av - dqsatdT*AT + gamma*Al
+#    f.savefig(var + '_%d.png' % ind)
 
 fig, ax = pyplot.subplots()
-Q = solutionDict['RKZ_qme']
-ax.plot(t, Q, '-r', label='Q')
+if ('RKZ_qme' in solutionDict.keys()):
+  Q = solutionDict['RKZ_qme']
+  ax.plot(t, Q, '-r', label='Q')
+  if ('RKZ_fac' in solutionDict.keys()):
+    f_n = solutionDict['RKZ_fac']
+    f_np1 = 2.0*f_n[1:]- f_n[0:-1]
+    f_np1 = np.where(f_np1 > 1.0, 1.0, f_np1)
+    f_np1 = np.where(f_np1 < 0.0, 0.0, f_np1)
+    ax2 = ax.twinx()
+    ax2.plot(t[1:], f_n[1:], '-b')
+    ax2.tick_params('y', colors='b')
+    term_A = None
+    term_B = None
+    term_C = None
+    if ('RKZ_term_A' in solutionDict.keys()):
+      term_A = solutionDict['RKZ_term_A']
+      ax.plot(t, term_A, '-k', label='in-cloudy condensation')
+    if ('RKZ_term_B' in solutionDict.keys()):
+      term_B = solutionDict['RKZ_term_B']
+      ax.plot(t, term_B, '-m', label='out-cloud evaporation')
+    if ('RKZ_term_C' in solutionDict.keys()):
+      term_C = solutionDict['RKZ_term_C']
+      term_CN = np.where(f_np1 > f_n[1:], term_C[1:], 0.0)
+      ax.plot(t[1:], term_CN, '-g', label='cloud nucleation')
+      term_CA = np.where(f_np1 < f_n[1:], term_C[1:], 0.0)
+      ax.plot(t[1:], term_CA, '-c', label='cloud annihilation')
+    if (not (term_A is None or term_B is None or term_C is None)):
+      ax.plot(t, term_A + term_B + term_C, '--r')
 
-#  f = solutionDict['RKZ_f']
-#  termA = f*(Av - dqsatdT*AT)/gamma
-#  termB = -(1.0 - f)*Al
-#  Q = (f*D/gamma - Al + dt*D/gamma*dfdRH*zforcing)/(1.0 + dt*D/gamma*dfdRH*zc3)
-#  termC = dt*D/gamma*dfdRH*( (Av - RH*dqsatdT*AT)/qsat - (1.0 + gam*RH*dqsatdT)/qsat*Q)
-#  print(np.amax(Q - (termA+termB+termC)))
-#  
-#  ax.plot(t, termA, '-r', label='term A')
-#  ax.plot(t, termB, '-g', label='term B')
-#  ax.plot(t, termC, '-b', label='term C')
-#  ax.plot(t, Q, '--k', label='Q')
 ax.legend()
-  
+ax.set_ylabel('condensation rate', fontsize='xx-large')
+ax2.set_ylabel('cloud fraction', fontsize='xx-large', color='b')
+
+
 pyplot.show()
+#fig.savefig('Q_%d.png' % ind)
+
