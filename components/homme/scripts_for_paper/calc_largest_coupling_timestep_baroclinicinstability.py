a87dc72d3fdab928b7400d21ee5b1e5e54dc8464
Chris Vogl <vogl2@llnl.gov>
2019-08-02 09:51:23 -0700

    added exceedance component and corresponding update to data

--- components/homme/scripts_for_paper/calc_largest_coupling_timestep_baroclinicinstability.py
+++ components/homme/scripts_for_paper/calc_largest_coupling_timestep_baroclinicinstability.py
@@ -9,23 +9,39 @@ if (len(sys.argv) < 2):
 outpath = sys.argv[1]
 
 methodList = ['ARS232', 'ARS222', 'ARS233', 'ARS343',
-              'ARS443', 'ARK324', 'ARK436', 'DBM453', 'SSP3333b',
+              'ARS443', 'ARK324', 'ARK436', 'DBM453', 'SSP3333b', 'SSP3333c',
               'IMEX-KG232a', 'IMEX-KG232b', 'IMEX-KG242a', 'IMEX-KG242b',
-              'IMEX-KG243b', 'IMEX-KG252a', 'IMEX-KG252b',
+              'IMEX-KG243a', 'IMEX-KG252a', 'IMEX-KG252b',
               'IMEX-KG253a', 'IMEX-KG254a', 'IMEX-KG254b',
               'IMEX-KG343a']
 
 dtList = [300, 270, 240, 216, 200, 192, 180, 160, 150, 135, 120, 100, 50, 20 ,10]
 
-# Iterate through methods and find acceptable timestep
+# read in reference solution for surface pressure
+fileRef = outpath + '/output_tsteptype5_tstep10_hydrostatic/dcmip2012_test41.nc'
+print 'Reading reference solution from ' + fileRef
+data = Dataset(fileRef)
+qRef = data['ps'][:]
+
+# read in acceptable solution for surface pressure
+fileRef = outpath + '/output_tsteptype5_tstep300_hydrostatic/dcmip2012_test41.nc'
+print 'Reading acceptable solution from ' + fileRef
+data = Dataset(fileRef)
+q = data['ps'][:]
+numElements = np.shape(q)[1]*np.shape(q)[2]
+errorTol = np.sqrt( np.sum((q-qRef)**2, axis=(1,2))/numElements )
+
+
+# Iterate through methods and find largest timestep
 largestDtDict = {}
-dtDict = {}
+exceedDict = {}
 for method in methodList:
   print method
   currentList = dtList + [] # make a copy of dtList
 
   success = False
   dt = np.amax(currentList)
+  exceed = np.inf
   while (not success):
     fileName = outpath + '/output_tsteptype%d_tstep%d/dcmip2012_test41.nc' \
                % (methodDict[method], dt)
@@ -40,13 +56,18 @@ for method in methodList:
           break
     else:
       print('... yes.')
+      q = data['ps'][:]
+      error = np.sqrt( np.sum((q-qRef)**2, axis=(1,2))/numElements )
+      ind = np.argmax(error-errorTol)
+      exceed = (error[ind]-errorTol[ind])/(error[ind]+1e-16)
       success = True
 
   largestDtDict[method] = dt
+  exceedDict[method] = exceed
 
 filename = './data/largest_coupling_timestep_baroclinicinstability.txt'
 print('Writing out ' + filename)
 fileobj = open(filename, 'w')
 for method in methodList:
-  fileobj.write('%s\t%d\n' % (method, largestDtDict[method]))
+  fileobj.write('%11s %3d %2.0f\n' % (method, largestDtDict[method], exceedDict[method]*100))
 fileobj.close()
