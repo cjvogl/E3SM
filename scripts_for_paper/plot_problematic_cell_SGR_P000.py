d076de386d2145ffd7d3346194f0a0bc3370197c
Chris Vogl <vogl2@llnl.gov>
2019-11-14 13:26:08 -0800

    updated problematic cell plot script

--- scripts_for_paper/plot_problematic_cell_SGR_P000.py
+++ scripts_for_paper/plot_problematic_cell_SGR_P000.py
@@ -1,5 +1,6 @@
 import matplotlib
 import matplotlib.pyplot as pyplot
+from matplotlib.lines import Line2D
 import numpy as np
 import os.path
 
@@ -44,18 +45,33 @@ tmp = np.loadtxt(txtFile)
 t = tmp[0,:]
 Al = tmp[1,:]
 
-fig, ax = pyplot.subplots(figsize=(11,10))
-ax.plot(t[2:], ql[2:], '-k', label='cloud liquid', linewidth=3)
-ax.plot(t[2:], dt*Al[2:], '-m', label='dt*(liq. tend.)', linewidth=3)
-ax.plot(t[2:], dt*(term_A[2:] + term_B[2:] + term_C[2:]), '--g', label='dt*(A+B+C)', linewidth=3)
-ax.plot(t[2:], dt*term_C[2:], ':r', label='dt*C', linewidth=3)
-ax2 = ax.twinx()
-ax2.plot(t[2:], f[2:], '-.b', linewidth=3)
-ax.set_xlabel('time (s)', fontsize='x-large')
-ax.set_ylabel('concentration (kg/kg)', fontsize='x-large')
-ax2.set_ylabel('cloud fraction', color='b', fontsize='x-large')
+fig, ax = pyplot.subplots(1, 2, sharex=True, sharey=True, figsize=(20,10))
+
+ax[0].plot(t[2:], dt*(term_A[2:] + term_B[2:] + term_C[2:]), '-g', label='dt*(A+B+C)', linewidth=3)
+ax[0].plot(t[2:], dt*term_C[2:], '-r', label='dt*C', linewidth=3)
+ax2 = ax[0].twinx()
+ax2.plot(t[2:], f[2:], '-b', linewidth=3)
+ax2.tick_params('y', colors='w')
+ax[0].set_xlabel('time (s)', fontsize='x-large')
+ax[0].set_ylabel('concentration (kg/kg)', fontsize='x-large')
+ax[0].set_ylim(-4.0e-8, 3.0e-8)
+handles, labels = ax[0].get_legend_handles_labels()
+handles.append(Line2D([], [], color='blue', linewidth=3, linestyle='-'))
+labels.append('cloud frac.')
+ax[0].legend(handles, labels, loc='lower right', fontsize='large')
+
+ax[1].plot(t[2:], ql[2:], '-k', label='cloud liquid', linewidth=3)
+ax[1].plot(t[2:], dt*Al[2:], '--m', label='dt*(liq. tend.)', linewidth=3)
+ax2 = ax[1].twinx()
+ax2.plot(t[2:], f[2:], '-b', linewidth=3)
 ax2.tick_params('y', colors='b')
-ax.legend(loc='upper right', fontsize='large')
+ax2.set_ylabel('fraction of grid cell', color='b', fontsize='x-large')
+ax[1].set_xlabel('time (s)', fontsize='x-large')
+handles, labels = ax[1].get_legend_handles_labels()
+handles.append(Line2D([], [], color='blue', linewidth=3, linestyle='-'))
+labels.append('cloud frac.')
+ax[1].legend(handles, labels, loc='lower right', fontsize='large')
+
 fig.tight_layout()
 
 fig.savefig('problematic_cell_P000.pdf')
